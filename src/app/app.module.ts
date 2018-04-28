180c2b57253f636628ba7069c4a7c44ae805235b
Sneha P P <sneha.pp@neoito.com>
2018-04-28 13:59:41 +0530

    added selectors in layout page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { TopbarComponent } from './component/topbar/topbar.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { LayoutComponent } from './container/layout/layout.component';
-
+import { LayoutModule } from './container/layout/layout.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TopbarComponent,
-    FooterComponent,
-    LayoutComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TopbarComponent, FooterComponent],
+  imports: [BrowserModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/container/layout/layout.module.ts
+++ src/app/container/layout/layout.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SidebarComponent } from '../../component/sidebar/sidebar.component';
+import { TopbarComponent } from '../../component/topbar/topbar.component';
+import { FooterComponent } from '../../component/footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    SidebarComponent,
+    TopbarComponent,
+    FooterComponent
+  ], 
 })
 export class LayoutModule { }
