761deed8aa337c2831013b1454fa8ed3dbc0afcb
daliyajohn <daliyajohn75@gmail.com>
2018-06-09 11:51:30 +0530

    Sample json and UI fix

--- src/app/component/sidebar/sidebar.component.ts
+++ src/app/component/sidebar/sidebar.component.ts
@@ -6,14 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
-  items = [
-    {
-      id: 1,
-      menu: 'menu1'
-    }
-  ];
+  s = {
+    theme: '', // background color
+    logo: '',
+    items: [
+      // menu items
+      {
+        id: 1,
+        menu: 'menu1',
+        url: '#',
+        icon: '',
+        toggleButton: ''
+      },
+      {
+        id: 2,
+        menu: 'menu2',
+        url: '#'
+      },
+      {
+        id: 23,
+        menu: 'menu3',
+        url: '#'
+      }
+    ],
+    breadcrumbs: [
+      // breadcrumbs
+      {
+        id: 1,
+        menu: 'b1',
+        url: '#',
+        icon: ''
+      },
+      {
+        id: 2,
+        menu: 'b2',
+        url: '#'
+      }
+    ]
+  };
   constructor() {
-    console.log(this.items);
+    console.log(this.s);
   }
 
   ngOnInit() {}
