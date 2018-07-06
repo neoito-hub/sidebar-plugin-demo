6697015c2d367818b9d652a34292a4addd066654
daliyajohn <daliyajohn75@gmail.com>
2018-07-06 11:11:17 +0530

    Change css file to scss file

--- src/app/component/content/content.component.ts
+++ src/app/component/content/content.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styleUrls: ['./content.component.scss']
 })
 export class ContentComponent implements OnInit {
   constructor() {}
