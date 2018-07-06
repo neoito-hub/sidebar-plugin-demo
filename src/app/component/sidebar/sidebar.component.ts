b1e1898fb2f21111e08a192a3476274bf05230e6
daliyajohn <daliyajohn75@gmail.com>
2018-07-06 14:22:51 +0530

    Add breadcrumbs UI

--- src/app/component/sidebar/sidebar.component.ts
+++ src/app/component/sidebar/sidebar.component.ts
@@ -10,24 +10,28 @@ export class SidebarComponent implements OnInit {
     theme: '', // background color
     logo: '',
     items: [
-      // menu items
       {
         id: 1,
-        menu: 'menu1',
-        url: '#',
-        icon: 'assignment_turned_in',
-        toggleButton: ''
-      },
-      {
-        id: 2,
-        menu: 'menu2',
+        menu: 'Dashboard',
         icon: 'card_travel',
         url: '#'
       },
       {
-        id: 23,
-        menu: 'menu3',
-        icon: 'event',
+        id: 2,
+        menu: 'Product',
+        icon: 'folder',
+        url: '#'
+      },
+      {
+        id: 3,
+        menu: 'Event',
+        icon: 'group',
+        url: '#'
+      },
+      {
+        id: 4,
+        menu: 'Settings',
+        icon: 'settings',
         url: '#'
       }
     ],
